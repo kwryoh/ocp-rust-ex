eb69d23ea514ae9647ffcee3cba1233b8f390d0d
Ryoh Kawai <kawairyo@rg-automated.jp>
2021-10-21 09:05:48 +0000

    Fix Sample HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,81 @@
 use anyhow::Result;
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener};
+
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+use serde_derive::Deserialize;
+
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+use futures_util::future::join;
+use std::convert::Infallible;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    #[serde(default="default_port")]
+    port: u16,
+    #[serde(default="default_second_port")]
+    second_port: u16,
+}
+
+fn default_port() -> u16 {
+    8080
+}
+
+fn default_second_port() -> u16 {
+    8888
+}
+
+// Sample handler
+async fn index(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    // Iterate request header
+    for (hkey, hval) in req.headers().iter() {
+        let hname = hkey.as_str().to_string().to_uppercase();
+        info!("Request Header: {}: {}", hname, hval.to_str().unwrap_or(""));
+    }
+
+    // Set response body
+    let response = match std::env::var("RESPONSE") {
+        Ok(val) => val,
+        Err(_) => "Hello, OpenShift!\n".to_string(),
+    };
+    info!("RESPONSE: {}", response);
+
+    // Return response
+    Ok(Response::new(Body::from(response)))
+}
+
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:8080").await?;
+    // Initial logging setting
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
 
-    loop {
-        let (mut socket, _) = listener.accept().await?;
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
 
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
+    // Get OS Environment
+    let config = envy::from_env::<Config>()?;
+    info!("Port={}", config.port);
+    info!("Second Port={}", config.second_port);
 
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    Ok(n) if n == 0 => return ,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprint!("failed to read from socket; err = {:?}", e);
-                        return ;
-                    }
-                };
+    let addr1 = ([0, 0, 0, 0], config.port).into();
+    let addr2 = ([0, 0, 0, 0], config.second_port).into();
 
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return ;
-                }
-            }
-        });
-    }
+    let srv1 = Server::bind(&addr1).serve(make_service_fn(|_| async {
+        Ok::<_, hyper::Error>(service_fn(index))
+    }));
+
+    let srv2 = Server::bind(&addr2).serve(make_service_fn(|_| async {
+        Ok::<_, hyper::Error>(service_fn(index))
+    }));
+
+    info!("Listening on http://{}", addr1);
+    info!("Listening on http://{}", addr2);
+
+    let _ret = join(srv1, srv2).await;
+
+    Ok(())
 }
